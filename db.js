c863cc5504e84de82ac143f4f8e57313d78bbaff
Onur Ergencicegi <onurergencicegiii@gmail.com>
2026-07-08 23:23:46 +0300

    Remote-Support guncellemesi

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const Database = require("better-sqlite3");
 
-const DB_PATH = path.join(__dirname, "..", "data", "auth.db");
+const DB_PATH = path.join(__dirname, "data", "auth.db");
 
 const db = new Database(DB_PATH);
 db.pragma("journal_mode = WAL");
